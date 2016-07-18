83956ebd9019a8ec0b118668258ae576101a685c
SeRoGu <seba@MacBook-Pro-de-Seba.local>
2016-07-18 11:57:30 -0300

    Inicialización

---
+++ EjemploKVO/EjemploKVO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EjemploKVO
+//
+//  Created by Seba on 18/7/16.
+//  Copyright © 2016 Seba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
